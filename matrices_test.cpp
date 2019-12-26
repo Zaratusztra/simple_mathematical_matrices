151e7cbd2da753a912667f92f3fee1b020fa2a68
Zaratusztra <nietzscheanin@gmail.com>
2019-12-26 20:51:55 +0100

    Im fighting to simply make a test ;_;

--- matrices_test.cpp
+++ matrices_test.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 #include "matrices.h"
+
+void set_column_with_random_integers(int M, int N; int** array)
+{
+    for(int i = 0; i < M; i++)
+    {
+        array[i] = new new int[N];
+        for (int j = 0; j < N; j++)
+            array[i][j]=rand % 100;
+    }
+}
 
 int main(int argc, char *argv[])
 {
     std::cout << "Test started." << std::endl;
+
+    srand(time(NULL));
     int** array_A = new int[2];
-    {4,-1, 2};
-    {-2,0,7};
-    Matrix<int> A = Matrix<int>(2, 3, array_A);
+    set_column_with_random_integers(2,3,array_A);
     int** array_B = new int[3];
-    {2,3,-1,5};
-    {11,0,0,-3};
-    {9,3,7,2};
-    Matrix<int> B = Matrix<int>(3, 4, array_B);
+    set_column_with_random_integers(3,4,array_B);
     int** array_C = new int[3];
-    {1,2,3,4};
-    {0,1,0,1};
-    {-4,-3,-2,-1};
+    set_column_with_random_integers(3,4,array_C);
+    
+
+    Matrix<int> A = Matrix<int>(2, 3, array_A);
+    Matrix<int> B = Matrix<int>(3, 4, array_B);
     Matrix<int> C = Matrix<int>(3, 4, array_C);
     try{
         std::cout << A.str_repr() << std::endl;
